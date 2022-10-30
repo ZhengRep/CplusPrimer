3d213df1b73a2745612627c6db0ddd4941237d54
ZhengLi <2726401288@qq.com>
2022-10-30 21:33:16 +0800

    test signed char and unsigned char

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
-	int a, b;
-	std::cin >> a >> b;
-	std::cout << "a = " << a << ",b = " << b << std::endl;
+	unsigned char c = -1;
+	signed char c2 = 257;
+
+	printf("%d\n", c);
+	printf("%d\n", c2); //c2 is not undefined and is zero
 	return 0;
 }
