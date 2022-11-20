9e75a5b32dd6b5dbf8250a847cd8a0c26071678f
ZhengLi <2726401288@qq.com>
2022-11-20 16:24:41 +0800

    add string

--- string/Source.cpp
+++ string/Source.cpp
@@ -16,11 +16,44 @@ int main()
 		cout << word << endl;
 	}*/
 
-	string line;
+	//string line;
 
-	while(getline(cin, line)){
-		cout << line << endl;
+	//while(getline(cin, line)){ //terminate newline character
+	//	cout << line << endl;
+	//}
+
+	/*string line;
+	string::size_type i = line.size();*/
+
+	/*
+	a range for:
+	for(declaration : expression)
+		statement
+	*/
+
+	string str("Some String!");
+	for (auto c : str) {
+	//for(char c : str){
+		c = toupper(c);
 	}
+	cout << str << endl;
+
+	/*
+	char c;
+	isalnum(c);
+	isalpha(c);
+	isdigit(c);
+	isgraph(c);
+	islower(c);
+	isprint(c);
+	ispunct(c);
+	isspace(c);
+	isupper(c);
+	tolower(c);
+	toupper(c);
+	*/
+
+
 
 	return 0;
 }
