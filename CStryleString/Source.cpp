f167792b4ec3fbb2d18f1f38a2739b30d739cdb2
ZhengLi <2726401288@qq.com>
2022-12-30 15:43:11 +0800

    add CStyleString

---
+++ CStryleString/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//char ca[] = { 'c', '+', 'c' };
+	//cout << strlen(ca) << endl; //no zero-terminated, so strlen(ca) is a random value
+
+	return 0;
+}
